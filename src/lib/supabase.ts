4578f0f3662b2ec98d0d40671026f9c8cef7daf3
sanny1 <sanyogcp@hotmail.com>
2026-01-20 21:07:25 +0000

    changed the environment variable

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,8 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY || '';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export const supabase = createClient(
-    supabaseUrl,
-    supabaseAnonKey
+    PUBLIC_SUPABASE_URL,
+    PUBLIC_SUPABASE_ANON_KEY
 );
